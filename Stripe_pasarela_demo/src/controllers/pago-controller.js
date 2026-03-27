fc5309d6b36a36ac307622af94c7639e2071d50e
Pegi <pegi@ucc.mx>
2026-03-26 19:44:01 -0600

    Proyecto Demo psarela Stripe

--- Stripe_pasarela_demo/src/controllers/pago-controller.js
+++ Stripe_pasarela_demo/src/controllers/pago-controller.js
@@ -1,4 +1,29 @@
-const crearSesionPago = (req, res) =>{
-    res.send("Pagando..");
+import Stripe from "stripe";
+import { SECRET_STRIPE } from "../config.js";
+
+const stripe = new Stripe(SECRET_STRIPE);
+
+const crearSesionPago = async (req, res) =>{
+    const sesion = await stripe.checkout.sessions.create({
+        line_items:[
+            {   
+                price_data: {
+                product_data:{
+                    name:'Pecera mediana',
+                    description: 'Tamanño de 16x7 cm',
+                },
+                currency:'mxn',
+                unit_amount: 1000,
+                },
+                quantity:4,
+            },// un producto para vender
+           
+        ],
+        mode:"payment",
+        success_url:'http://localhost:4000/exito',
+        cancel_url:'http://localhost:4000/cancelado',
+    });
+    res.json(sesion);
 };
+
  export default crearSesionPago;
