dc40484d888fc2012a8f6f35382eca7fd86f0ee5
Pegi <pegi@ucc.mx>
2026-04-21 20:01:48 -0600

    Reintegracion

---
+++ Demo_React/DemoSesion/src/cookie/AppCookie.jsx
@@ -0,0 +1,24 @@
+import {useAuthCookie} from './useAuthCookie';
+
+
+export default function AppCookie(){
+    const { token, login, logout } = useAuthCookie();
+
+    return(
+        <div>
+            <h2>Auth con Cookies</h2>
+            {
+                token ? (
+                    <>
+                    <p>Sesión con cookie Activa</p>
+                    <button onClick={logout}>Logout</button>
+                    </>
+                ) : (
+                    <>
+                        <button onClick={login}>Login</button>
+                    </>
+                )
+            } 
+        </div>
+    );
+ }
